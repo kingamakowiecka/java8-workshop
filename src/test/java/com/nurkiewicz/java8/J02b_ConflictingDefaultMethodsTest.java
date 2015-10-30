fb0b6748831ef146c7bc3f17ef3ee642801120a8
Kinga Makowiecka <kinga.makowiecka@payu.pl>
2015-10-30 22:53:15 +0100

    Default methods exercise

--- src/test/java/com/nurkiewicz/java8/J02b_ConflictingDefaultMethodsTest.java
+++ src/test/java/com/nurkiewicz/java8/J02b_ConflictingDefaultMethodsTest.java
@@ -4,7 +4,6 @@ import com.nurkiewicz.java8.defmethods.Engine;
 import com.nurkiewicz.java8.defmethods.Job;
 import com.nurkiewicz.java8.defmethods.Lifecycle;
 import com.nurkiewicz.java8.defmethods.RuleEngine;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -14,22 +13,21 @@ import static org.fest.assertions.api.Assertions.assertThat;
  * First let {@link RuleEngine} implement all three interfaces.
  * Then decide which of the conflicting implementations should be chosen.
  */
-@Ignore
 public class J02b_ConflictingDefaultMethodsTest {
 
-	private final RuleEngine ruleEngine = new RuleEngine();
+    private final RuleEngine ruleEngine = new RuleEngine();
 
-	@Test
-	public void shouldExtendFewInterfaces() {
-		assertThat(ruleEngine).isInstanceOf(Job.class);
-		assertThat(ruleEngine).isInstanceOf(Engine.class);
-		assertThat(ruleEngine).isInstanceOf(Lifecycle.class);
-	}
+    @Test
+    public void shouldExtendFewInterfaces() {
+        assertThat(ruleEngine).isInstanceOf(Job.class);
+        assertThat(ruleEngine).isInstanceOf(Engine.class);
+        assertThat(ruleEngine).isInstanceOf(Lifecycle.class);
+    }
 
-	@Test
-	public void shouldReturnValueFromJob() {
-		assertThat(ruleEngine.start()).isEqualTo("Job");
-	}
+    @Test
+    public void shouldReturnValueFromJob() {
+        assertThat(ruleEngine.start()).isEqualTo("Job");
+    }
 
 }
 
